68f0bc0ea424cc2530d74ff881483195bcf255ed
dkaaven <daniel@kaaven.se>
2026-06-30 21:23:36 +0200

    Changed build source to my repo

--- ct/eurooffice.sh
+++ ct/eurooffice.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+source <(curl -fsSL https://raw.githubusercontent.com/dkaaven/ProxmoxVE/main/misc/build.func)
 # Copyright (c) 2021-2026 community-scripts ORG
 # Author: Daniel Kåven (dkaaven)
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
